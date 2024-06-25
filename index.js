143bebdf910f1056a06c31ab9036379399252309
huska0707 <hussaink001haled@gmail.com>
2024-06-25 15:42:52 -0500

    ethical ai init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
